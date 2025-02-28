bab8a15d2dd42e0585314f8b1e29fd8ed66c3f4c
Joshua-1101 <111673587+Joshua-1101@users.noreply.github.com>
2025-02-28 15:22:03 +0800

    my first commit

---
+++ bytevote/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+//components
+import SideBar from './components/SideBar'
+function App() {
+  
+  return (
+    <>
+        <SideBar />
+    </>
+  )
+}
+
+export default App
---
+++ bytevote/src/components/SideBar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function SideBar() {
+  return (
+    <aside>
+        {/* side bar */}
+        <h2>Side Bar</h2>
+        <ul>
+            <li>Link 1</li>
+            <li>Link 2</li>
+            <li>Link 3</li>
+        </ul>
+    </aside>
+  )
+}
+
+export default SideBar
