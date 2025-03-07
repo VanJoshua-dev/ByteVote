2e6d016ca4c2a216620b6c41d018dee000761fc1
Joshua-1101 <111673587+Joshua-1101@users.noreply.github.com>
2025-03-07 15:58:47 +0800

    fix frontend bug

--- bytevote/src/main.jsx
+++ bytevote/src/main.jsx
@@ -3,11 +3,10 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, BrowserRouter as Router } from "react-router-dom";
 import App from './App';
 import './index.css';
-import DashBoard from './pages/DashBoard';
-import SideBar from './components/SideBar';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <Router>
         <App />
-  </BrowserRouter>
+  </Router>
 );
