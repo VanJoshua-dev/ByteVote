a12f698295d3ce31c6ee1745e35c7330a79abde1
Joshua-1101 <111673587+Joshua-1101@users.noreply.github.com>
2025-03-07 13:47:02 +0800

    change

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 
 require('dotenv').config();
 
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcryptjs");
 const express = require('express');
 const mysql = require('mysql2');
 const cors = require('cors');
