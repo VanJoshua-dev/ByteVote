a3f3a5f8c6ffd79e0b1a3bf17f0a37c3729cdb40
Joshua-1101 <111673587+Joshua-1101@users.noreply.github.com>
2025-03-09 16:35:17 +0800

    fix request origin

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 
 // ✅ CORS Configuration
 const corsOptions = {
-    origin: "*",
+    origin: "https://bytevote.onrender.com",
     methods: "GET, POST, PATCH, DELETE, OPTIONS",
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
